5f8eff3163f3d0c6e5887a1c09fc8d516cbfd25f
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-05-28 12:34:10 -0600

    Create route working

--- controllers/PostController.rb
+++ controllers/PostController.rb
@@ -10,8 +10,28 @@ class PostController < ApplicationController
 		erb :post_index
 	end 
 
+# Create route working 
+
+	post '/' do 
+
+		new_post = Post.new 
+
+		new_post.photo_url   = params[:photo_url]
+		new_post.f_name      = params[:f_name]
+		new_post.l_name      = params[:l_name]
+		new_post.period	     = params[:period]
+		new_post.description = params[:description]
+
+		new_post.save
+
+		redirect '/posts'
+	end
+
+# New route working 
 	get '/new' do 
 
 		erb :post_new
 	end
+
 end 
+
