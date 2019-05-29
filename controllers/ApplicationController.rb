14f5fe32b40533b2e1d59cc295bdc1aaee2d1ea7
Tania Medina <tanivelisse@gmail.com>
2019-05-29 10:50:16 -0500

    user controller delete

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -1,24 +1,23 @@
-
-
-
 class ApplicationController < Sinatra::Base
   require 'bundler'
   Bundler.require()
 
-   ActiveRecord::Base.establish_connection(
+  enable :sessions
+
+  ActiveRecord::Base.establish_connection(
     :adapter => 'postgresql',
     :database => 'women_in_tech'
   )
   set :views, File.expand_path('../../views', __FILE__)
 
- get '/' do 
+  get '/' do 
     "Monumental Women in Tech."
   end 
 
 
- get'/test' do 
-	binding.pry
-end
+  get'/test' do 
+    binding.pry
+  end
 
 
 end 
---
+++ controllers/UserController.rb
@@ -0,0 +1,98 @@
+class UserController < ApplicationController
+
+
+# get // register & login page
+	get '/register_login' do 
+		erb :register_login
+  end
+
+# create // register user
+	post '/register' do
+
+		user = User.find_by username: params[:username]
+		
+		if !user
+
+		user = User.new
+		user.username = params[:username]
+		user.password = params[:password]
+
+		user.save
+
+		session[:logged_in] = true
+	    session[:username] = user.username
+	    session[:message] = {
+	        success: true,
+	        status: "good",
+	        message: "Welcome #{user.username}."
+	    }
+
+	    redirect '/posts'
+
+	    else
+
+	    session[:message] = {
+	    	success: false,
+	    	status: "bad",
+	    	message: "Sorry, username #{params[:username]} is already taken."
+	    }
+
+	    redirect '/user/register_login' 
+		end
+
+	end
+
+#create // login user
+	post '/login' do
+		user = user.find_by username: params[:username]
+		password = params[:params]
+
+		if user && user.authenticate(password)
+
+		session[:logged_in] = true
+		session[:username] = user.username
+		session[:message] = {
+			success: true,
+			status: "good",
+			message: "Welcome back #{user.username}"
+		}
+
+		redirect '/posts'
+
+		else
+      
+		session[:message] = {
+		success: false,
+		status: "bad",
+		message: "Invalid username or password."
+		}
+      
+    
+      	redirect '/user/register_login'
+    	end
+
+	end
+
+# destroy // logout
+
+get '/logout' do
+
+	username = session[:username] # grab username before destroying session...
+
+    session.destroy
+
+    session[:message] = {
+      success: true,
+      status: "neutral",
+      message: "User #{username} logged out." #...so that we can use it here
+    }
+
+    redirect '/users/login'
+
+  end
+
+
+
+
+	
+end
