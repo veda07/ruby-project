78b2adeaac71ac206bb7deacfaaf9eaabbf5e256
Tania Medina <tanivelisse@gmail.com>
2019-05-30 16:00:50 -0500

    fixed bug

--- Config/environments.rb
+++ Config/environments.rb
@@ -2,7 +2,7 @@ configure :production, :development do
 	# setting up the database/ if there is URL use 1st one, or if it's local do 2nd one
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/women_of_tech')
 
-	ActiveRecord: :Base.establish_connection(
+	ActiveRecord::Base.establish_connection(
 		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme, 
 		:host => db.host,
 		:username => db.user,
