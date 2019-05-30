5928321afcf2e1a9d25f734236263f7bad7f5dda
Tania Medina <tanivelisse@gmail.com>
2019-05-30 16:02:37 -0500

    fixed poss bug

--- Config/environments.rb
+++ Config/environments.rb
@@ -6,7 +6,7 @@ configure :production, :development do
 		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme, 
 		:host => db.host,
 		:username => db.user,
-		:password => db.password
+		:password => db.password,
 		:database => db.path[1..-1],
 		:encoding => 'utf8'
 	)
